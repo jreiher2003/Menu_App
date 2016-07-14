cd2fa5651d5707496085a94014528f3a0a10910c
Jeff Reiher <jreiher2003@yahoo.com>
2016-07-14 02:20:23 +0000

    Add provisions to pg_config.sh

--- pg_config.sh
+++ pg_config.sh
@@ -1,7 +1,9 @@
 apt-get -qqy update 
 apt-get -qqy install postgresql python-psycopg2
+apt-get -y install libq-dev 
+apt-get -y install python-dev
 apt-get -qqy install python-flask python-sqlalchemy
-apt-get -qqy intall python-pip
+apt-get -qqy install python-pip
 
 pip install us
 pip install slugify

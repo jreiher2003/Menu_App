8a185178b23d34a93a9a34b4b8905477dd82ad64
Jeff Reiher <jreiher2003@yahoo.com>
2016-07-23 09:37:16 -0400

    Remove pip install commands

--- pg_config.sh
+++ pg_config.sh
@@ -1,3 +1,4 @@
+#!/bin/bash 
 apt-get -qqy update 
 apt-get -qqy install postgresql python-psycopg2
 apt-get -y install libq-dev 
@@ -5,9 +6,5 @@ apt-get -y install python-dev
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
 
-pip install us
-pip install slugify
-pip install converage
-pip install Flask-Migrate
-pip install Flask-Testing 
+ 
 
